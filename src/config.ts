496b296752424c131abc0ed4bbbf26d407bea09f
Orliluq <orlibet@gmail.com>
2024-05-01 19:08:56 -0400

    feat: bloque call to action del sitio

---
+++ src/config.ts
@@ -0,0 +1,19 @@
+export const SiteMetadata = {
+  title: 'Tycatch Factory Site',
+  description: 'Desarrollo de Software a Medida. Creamos aplicaciones personalizadas que se adaptan perfectamente a tus necesidades.',
+  author: {
+    name: 'Tycatch_tv',
+    twitter: '@trycatch_tv',
+    url: 'https://trycatch.tv/',
+    email: 'judlup@trycatch.tv',
+    whatsappNumber: '+573164907627',
+  },
+  org: {
+    name: 'Tycatch Factory',
+    twitter: '@trycatch_tv',
+    url: 'https://trycatchfactory.com',
+    email: 'judlup@trycatch.tv',
+    description:
+      'Somos una comunidad de desarrolladores apasionados y expertos dedicados a crear soluciones de software innovadoras que no solo cumplen, sino que superan las expectativas de nuestros clientes.'
+  },
+};
