24814cc4dcf858b7c0c09da79327e3af71ea3f18
Manu Barrios <manuelcar96@outlook.com>
2024-05-08 20:14:35 -0500

    Se cambia el color del navbar y se deja el diseño del mockup

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -14,7 +14,7 @@ export default {
         "tcf-gray": "#B5B5B5",
         "tcf-gray-l": "#F2F2F2",
         "tcf-gray-foot":"#535353",
-        "tcf-blue": "#172544",
+        "tcf-blue": "#172554",
       },
       width:{
         "60":"60px",
