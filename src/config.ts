1dbe86efdffd48047c630335a0cf12353dc40204
angel <angelxd0714@gmail.com>
2024-05-23 01:35:48 -0500

    fix: se configura y se hace la primera parte de carga de datos.json para services

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
+import { z } from "astro/zod";
+import { defineCollection } from "astro:content";
+
 export const SiteMetadata = {
   title: 'Tycatch Factory Site',
   description: 'Desarrollo de Software a Medida. Creamos aplicaciones personalizadas que se adaptan perfectamente a tus necesidades.',
@@ -16,4 +19,25 @@ export const SiteMetadata = {
     description:
       'Somos una comunidad de desarrolladores apasionados y expertos dedicados a crear soluciones de software innovadoras que no solo cumplen, sino que superan las expectativas de nuestros clientes.'
   },
+};
+const serviceCollection = defineCollection({
+  type:"data",
+  schema: z.object({
+    desarrolloWeb: z.object({
+      namePage: z.string(),
+      title: z.string(),
+      description: z.string(),
+      levelEstructureTeam: z.array(z.string()),
+    }),
+    desarrolloMmobile: z.object({
+      namePage: z.string(),
+      title: z.string(),
+      description: z.string(),
+      levelEstructureTeam: z.array(z.string()),
+    })
+  }),
+});
+
+export const collections = {
+  service: serviceCollection,
 };
