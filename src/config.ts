c32490f07e5d5925034024fcbc40094c93b8809e
angel <angelxd0714@gmail.com>
2024-05-23 20:32:37 -0500

    se corrije json de services

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
-import { z } from "astro/zod";
-import { defineCollection } from "astro:content";
 
 export const SiteMetadata = {
   title: 'Tycatch Factory Site',
@@ -20,54 +18,5 @@ export const SiteMetadata = {
       'Somos una comunidad de desarrolladores apasionados y expertos dedicados a crear soluciones de software innovadoras que no solo cumplen, sino que superan las expectativas de nuestros clientes.'
   },
 };
-const serviceCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    desarrolloWeb: z.object({
-      namePage: z.string(),
-      title: z.string(),
-      description: z.string(),
-      levelEstructureTeam: z.array(z.string()),
-    }),
-    desarrolloMmobile: z.object({
-      namePage: z.string(),
-      title: z.string(),
-      description: z.string(),
-      levelEstructureTeam: z.array(z.string()),
-    })
-  }),
-});
-const projectsCollections = defineCollection({
-  type: "data",
-  schema: z.object({
-    sistemaInventario: z.object({
-      title: z.string(),
-      description: z.string(),
-      characteristics: z.array(z.string())
 
-    }),
-    sistemaFacturacion: z.object({
-      title: z.string(),
-      description: z.string(),
-      characteristics: z.array(z.string())
-
-    })
-
-  })
-})
-const sectionsCollection = defineCollection({
-  type: "data",
-  schema: z.object({
-    "call-to-action": z.object({
-      title: z.string(),
-      description: z.string(),
-      // Agrega otros campos que necesites
-    })
-  })
-});
-export const collections = {
-  service: serviceCollection,
-  project: projectsCollections,
-  sections: sectionsCollection,
-};
 
