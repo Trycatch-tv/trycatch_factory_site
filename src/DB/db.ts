ee375a95dfd7805115e580b0b1327a63ec2b063e
angelxd0714 <angelxd0714@gmail.com>
2024-06-12 01:21:13 +0000

    add:se mejora performance

--- src/DB/db.ts
+++ src/DB/db.ts
@@ -1,9 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = import.meta.env.PUBLIC_API_URL
-const supabaseKey = import.meta.env.PUBLIC_API_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
+import { createClient } from '@supabase/supabase-js';
 
+const supabaseUrl = import.meta.env.PUBLIC_API_URL;
+const supabaseKey = import.meta.env.PUBLIC_API_KEY;
 
-export { supabase }
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error('Supabase URL and Key are required');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
 
 
