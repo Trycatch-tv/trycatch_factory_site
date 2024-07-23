c4d1daa8ab785eaafc016052a764a9a59d2709c4
angel <angelxd0714@gmail.com>
2024-07-23 15:16:29 -0500

    add:se hace cambios por otro lado

--- src/hooks/useHandleSubmit.ts
+++ src/hooks/useHandleSubmit.ts
@@ -1,10 +1,6 @@
 import { sendData } from "../service/postData";
-import authories from "../service/authGoogle";
-import { createSpreadsheet, googleSheetSave, insertHeader } from "../service/googlesheet";
 
-const auth = await authories.authorize();
-const spreadsheetId = await createSpreadsheet(auth);
-await insertHeader(auth, spreadsheetId);
+
 
 
 const successComponent = `<div class="flex items-center p-4 mb-4 text-sm text-green-800 border border-green-300 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400 dark:border-green-800" role="alert"><svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/></svg><span class="sr-only">Info</span><div><span class="font-medium">Success alert!</span> the form has been sent successfully</div></div> `;
@@ -25,7 +21,7 @@ export const useSubmitHandler = () => {
   
       try {
         const response = await sendData(dataPost);
-        await googleSheetSave(dataPost, auth, spreadsheetId);
+      
         if (response.status === 201) {
           alertContainer.innerHTML = successComponent;
           form.reset();
