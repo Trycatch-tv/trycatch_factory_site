75c19593cf50de4529e1543262d7c1e569c90e0c
angelxd0714 <angelxd0714@gmail.com>
2024-07-26 03:39:50 +0000

    add:se configura traduccion

---
+++ src/i18n/ui.ts
@@ -0,0 +1,54 @@
+export const lenguages = {
+    en: "English",
+    es: "Spanish"
+}
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        "select.language": "Español",
+        "nav.Home": "Inicio",
+        "nav.Services": "Servicios",
+        "nav.Portfolio": "Portafolio",
+        "nav.Resources": "Recursos",
+        "nav.Team": "Equipo",
+        "nav.Blog": "Blog",
+        "nav.Contact": "Contacto",
+        "services.software-development": "Desarrollo de Software",
+        "services.technology-consulting": "Consultoría Tecnológica",
+        "services.Maintenance-Updates": "Mantenimiento y Actualización",
+        "services.Integrations": "Integraciones",
+        "services.Integrations-with-shopify-and-salesforce": "Integraciones a Shopify / Salesforce",
+        "services.landing-page": "Landing Page",
+        "portfolio.Inventory-System": "Sistema Inventario",
+        "portfolio.Billing-System": "Sistema Facturacion",
+        "titleHeroLanding":"Transformamos tus ideas en software personalizado",
+        "descriptionHeroLanding":"Diseñamos y desarrollamos soluciones tecnológicas a medida para llevar tu negocio al siguiente nivel. Nuestro enfoque en la calidad, la innovación y la experiencia del cliente garantiza resultados excepcionales.",
+        "titleButton":"Contáctanos Ahora",
+        "titleButton2":"Nuestras Soluciones"
+    },
+    en: {
+        "select.language": "English",
+        "nav.Home": "Home",
+        "nav.Services": "Services",
+        "nav.Portfolio": "Portafolio",
+        "nav.Resources": "Resources",
+        "nav.Team": "Team",
+        "nav.Blog": "Blog",
+        "nav.Contact": "Contact",
+        "services.software-development": "Software Development",
+        "services.technology-consulting": "Technology Consulting",
+        "services.Maintenance-Updates": "Maintenance and Updates",
+        "services.Integrations": "Integrations",
+        "services.Integrations-with-shopify-and-salesforce": "Integrations with Shopify / Salesforce",
+        "services.landing-page": "Landing Page",
+        "portfolio.Inventory-System": "Inventory System",
+        "portfolio.Billing-System": "Billing System",
+        "titleHeroLanding":"Transforming your ideas into software customized",
+        "descriptionHeroLanding":"We design and develop innovative solutions to transform your business into the next level. Our focus on quality, innovation, and customer experience ensures exceptional results.",
+        "titleButton":"Contact Us Now",
+        "titleButton2":"Our Solutions"
+
+    },
+
+} as const;
